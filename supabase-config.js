1911004425a5041add6318b8fb29157735c08944
jeanlouis22 <guejeanlouis5@gmail.com>
2026-08-05 21:55:52 +0000

    Fix SUPABASE_PUBLISHABLE_KEY string formatting

--- supabase-config.js
+++ supabase-config.js
@@ -1,6 +1,6 @@
 const SUPABASE_URL = "https://hrvgmnvkynjmtfxfylvr.supabase.co";
 
-const SUPABASE_PUBLISHABLE_KEY = sb_publishable_yOtrUEaPh6zv8oCTh12QMA_bd__hs5S";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_yOtrUEaPh6zv8oCTh12QMA_bd__hs5S";
 
 const supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
